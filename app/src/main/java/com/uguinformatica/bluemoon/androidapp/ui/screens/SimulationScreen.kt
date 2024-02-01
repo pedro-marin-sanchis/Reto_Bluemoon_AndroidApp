c9004108f4315404a9baaf2ee3ed4f957c0a3928
Javier <alubla1033@ieselcaminas.org>
2024-02-01 20:34:37 +0100

    ST-45 Modal Navigaton View finished

--- app/src/main/java/com/uguinformatica/bluemoon/androidapp/ui/screens/SimulationScreen.kt
+++ app/src/main/java/com/uguinformatica/bluemoon/androidapp/ui/screens/SimulationScreen.kt
@@ -28,9 +28,6 @@ import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
