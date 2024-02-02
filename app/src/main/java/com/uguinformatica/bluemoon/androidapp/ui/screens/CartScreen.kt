922dcffde05692b9958a256d7b12d5caff7e4d0f
Javier <alubla1033@ieselcaminas.org>
2024-02-02 19:17:33 +0100

    ST-49 cart screen finished

--- app/src/main/java/com/uguinformatica/bluemoon/androidapp/ui/screens/CartScreen.kt
+++ app/src/main/java/com/uguinformatica/bluemoon/androidapp/ui/screens/CartScreen.kt
@@ -1,11 +1,16 @@
 package com.uguinformatica.bluemoon.androidapp.ui.screens
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
 import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
 import androidx.compose.foundation.lazy.staggeredgrid.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.uguinformatica.bluemoon.androidapp.R
 import com.uguinformatica.bluemoon.androidapp.domain.models.Product
@@ -13,16 +18,31 @@ import com.uguinformatica.bluemoon.androidapp.ui.components.CartComponents.CartP
 
 @Composable
 fun CartScreen(paddingValues: PaddingValues) {
-    LazyVerticalStaggeredGrid(
-        columns = StaggeredGridCells.Fixed(1),
-        content = {
-            items(getProducts()) { index ->
-                CartProductItem(product = index)
-            }
-        },
-        modifier = Modifier.padding(paddingValues),
-        userScrollEnabled = true
-    )
+    Column(
+        modifier = Modifier
+            .padding(paddingValues)
+            .fillMaxSize()
+    ) {
+
+        LazyVerticalStaggeredGrid(
+            columns = StaggeredGridCells.Fixed(1),
+            content = {
+                items(getProducts()) { index ->
+                    CartProductItem(product = index)
+                }
+                item {
+                    Button(onClick = { /*TODO*/ }) {
+                        Text(text = "Checkout")
+                    }
+                }
+            },
+            userScrollEnabled = true
+        )
+
+    }
+
+
+
 }
 
 private fun getProducts(): List<Product> {
