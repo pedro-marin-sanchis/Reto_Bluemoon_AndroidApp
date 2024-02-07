3694fba8fa60a38b651fc20d10d16efa31db10cf
Javier <alubla1033@ieselcaminas.org>
2024-02-07 16:49:53 +0100

    ST-62 Cart and Product View Model

--- app/src/main/java/com/uguinformatica/bluemoon/androidapp/ui/viewmodels/CartViewModel.kt
+++ app/src/main/java/com/uguinformatica/bluemoon/androidapp/ui/viewmodels/CartViewModel.kt
@@ -3,14 +3,17 @@ package com.uguinformatica.bluemoon.androidapp.ui.viewmodels
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.uguinformatica.bluemoon.androidapp.domain.models.CartItem
 
 class CartViewModel : ViewModel() {
 
+    private var _cartItemsList = MutableLiveData(listOf<CartItem>())
     private var _quantity = MutableLiveData("")
     private var _openAlertDialogDelete = MutableLiveData(false)
     private var _openModifyQuantityDialog = MutableLiveData(false)
     private var _openAlertDialogConfirm = MutableLiveData(false)
 
+    val cartItemsList: LiveData<List<CartItem>> = _cartItemsList
     val quantity: LiveData<String> = _quantity
     val openAlertConfirm: LiveData<Boolean> = _openAlertDialogConfirm
     val openAlertDelete: LiveData<Boolean> = _openAlertDialogDelete
