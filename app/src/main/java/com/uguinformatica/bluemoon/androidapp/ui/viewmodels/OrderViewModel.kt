60a4bf4de9d8b45a54a07880a5c51661cd6a6b52
Javier <alubla1033@ieselcaminas.org>
2024-02-07 16:46:01 +0100

    ST-62 Order View Model

--- app/src/main/java/com/uguinformatica/bluemoon/androidapp/ui/viewmodels/OrderViewModel.kt
+++ app/src/main/java/com/uguinformatica/bluemoon/androidapp/ui/viewmodels/OrderViewModel.kt
@@ -1,4 +1,16 @@
 package com.uguinformatica.bluemoon.androidapp.ui.viewmodels
 
-class OrderViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.uguinformatica.bluemoon.androidapp.domain.models.Order
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class OrderViewModel : ViewModel() {
+
+    private var _ordersList = MutableLiveData(listOf<Order>())
+
+    val ordersList: LiveData<List<Order>> = _ordersList
+
 }
